6cff6ee23d21fd49ca92457dfd4e69f2d7c6b939
Pouya Ahmadvand <pouya.ahmadvand@gmail.com>
2024-08-01 17:01:11 -0700

    update heatmap

--- cell_classifier.py
+++ cell_classifier.py
@@ -66,7 +66,8 @@ class CellClassifier:
             gradcam_resized = np.array(Image.fromarray(gradcams[i]).resize((self.patch_size, self.patch_size), Image.BILINEAR))
             x_end = self.patch_size if (x_pos + self.patch_size) < width else width - x_pos
             y_end = self.patch_size if (y_pos + self.patch_size) < height else height - y_pos
-            heatmap[y_pos:y_pos + y_end, x_pos:x_pos + x_end] += gradcam_resized[:y_end, :x_end]
+            if labels[i] == 1:
+                heatmap[y_pos:y_pos + y_end, x_pos:x_pos + x_end] += gradcam_resized[:y_end, :x_end]
         return labels, probabilities
 
     
